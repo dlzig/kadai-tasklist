c493b0339cd2ec7f042997378205a9e6329881b0
arisa.satomura <dl.zig7@gmail.com>
2020-04-24 14:30:54 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
-Rails.application.routes.draw do
-  root to: 'tasks#index'
+Rails.application.routes.draw do  get 'toppage/index'
+  root to: 'toppages#index'
+
+  get 'toppages/index'
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
 
   resources :tasks
-  
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
