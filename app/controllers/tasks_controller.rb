238b112d89b388712e964ffe01ee0737b540ba07
arisa.satomura <dl.zig7@gmail.com>
2020-04-25 08:37:19 +0000

    user task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(15)
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+    # @tasks = Task.order(id: :desc).page(params[:page]).per(15)
   end
   
   def show
@@ -13,12 +19,12 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desk).page(params[:page])
       flash.now[:danger] = 'Task が登録されませんでした'
       render :new
     end
@@ -39,7 +45,6 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
@@ -54,7 +59,12 @@ private
 # Stronge params
   def task_params
     params.require(:task).permit(:content, :status)
-
   end
-  
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do  get 'toppage/index'
-  root to: 'toppages#index'
+  root to: 'tasks#index'
 
   # URLの見栄えを考慮して個別にルーティングを設定
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :tasks
-
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks
 end
